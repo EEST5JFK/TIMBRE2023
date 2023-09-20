ca4462a6ca6d51f8628c7e2e8a1c92d95cca61a8
EEST5JFK <145466934+EEST5JFK@users.noreply.github.com>
2023-09-20 16:59:30 -0300

    Add files via upload

--- TIMBRE/TIMBRE.ino
+++ TIMBRE/TIMBRE.ino
@@ -5,56 +5,88 @@
 // Declaramos un RTC DS3231
 RTC_DS3231 rtc;
 
+#define RELAY 2
+#define LED_PILA 3
+#define ZUMBADOR 4
+#define LED_TIMBRE 5
+#define PANICO 6
+
+
 void setup () {
- Serial.begin(9600);
+    Serial.begin(9600);
 
- delay(3000);
+    delay(3000);
 
-pinMode(2,OUTPUT);
-pinMode(3,OUTPUT);
-pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
+    pinMode(RELAY,OUTPUT);
+    pinMode(LED_PILA,OUTPUT);
+    pinMode(ZUMBADOR,OUTPUT);
+    pinMode(LED_TIMBRE,OUTPUT);
+    pinMode(PANICO,INPUT);
 
 
+    // Comprobamos si tenemos el RTC conectado
+    if (! rtc.begin()) {
+      reemplazarPila();
 
- // Comprobamos si tenemos el RTC conectado
- if (! rtc.begin()) {
-   Serial.print("REEMPLAZAR BATERIA");
-   digitalWrite(4,LOW);
-   digitalWrite(3,HIGH);
-   while (1);
- }
+    }
 
- digitalWrite(4,HIGH);
 
  // Ponemos en hora, solo la primera vez, luego comentar y volver a cargar.
  // Ponemos en hora con los valores de la fecha y la hora en que el sketch ha sido compilado.
   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
-  //int horas[6]={7,9,9,10,11,12};
-  //int minutos[6]={40,40,50,50,0,0};
-
 }
 
 void loop () {
- DateTime now = rtc.now();
-  int i = 0;
-  int horarios = 6;
-    int horas[6]={7,9,9,10,11,11};
-  int minutos[6]={40,40,50,50,0,24};
-  String mensajes[6]={"ENTRADA TM","1ER RECREO","FIN 1ER RECREO","2DO RECREO","FIN 2DO RECREO","SALIDA TM"};
-    for(i=0;i<horarios;i++) {
-      if((horas[i]==now.hour())&&(minutos[i]==now.minute())) {
-         Serial.print(mensajes[i]);
-          digitalWrite(2,HIGH);
-          digitalWrite(5,HIGH);
-          delay(20000);
-          Serial.print("");
-          digitalWrite(2,LOW);
-          digitalWrite(5,LOW);
-          delay(60000);
-      }
+    DateTime now = rtc.now();
+
+    String horario = String(now.hour(), DEC) + ":" + String(now.minute(),DEC);
+    
+    //*******ESTO ES POR SI NO ANDA EL RELOJ*******
+    if(!now.isValid()) {
+      Serial.println("FALLA EN MODULO RTC");
+      tone(ZUMBADOR, 880);
+      delay(100);
+      noTone(ZUMBADOR);
+
     }
-   
-   delay(10000);
+    //*********************************************
+
+    //**********TURNO MAÑANA FRENTE**********
+    if(horario=="16:58"){
+        Serial.println("HORARIO DE PRUEBA");
+        tocarTimbre();
+    }
+              
+    //******************************************
+    delay(10000);
 }
+
+void tocarTimbre() {
+
+    digitalWrite(RELAY,HIGH);
+    digitalWrite(LED_TIMBRE,HIGH);
+    tone(ZUMBADOR, 880);
+    delay(500);
+    noTone(ZUMBADOR);
+    delay(29500);
+    digitalWrite(RELAY,LOW);
+    digitalWrite(LED_TIMBRE,LOW);
+    noTone(ZUMBADOR);
+    delay(40000);
+
+}
+
+void reemplazarPila() {
+  digitalWrite(LED_PILA,HIGH);
+  Serial.println("REEMPLAZAR PILA");
+  while(1) {
+      tone(ZUMBADOR, 880);
+      delay(250);
+      noTone(ZUMBADOR);
+      delay(250);
+
+  }
+
+}
+
